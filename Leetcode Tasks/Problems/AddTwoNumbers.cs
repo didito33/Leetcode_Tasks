94024656d554d64949b0f632bafba87bbb0405be
Gamer <Gamer@DESKTOP-24AJLS0>
2025-10-17 00:19:41 +0300

    Add project files.

---
+++ Leetcode Tasks/Problems/AddTwoNumbers.cs
@@ -0,0 +1,37 @@
+﻿using Leetcode_Tasks.Models;
+
+namespace Leetcode_Tasks.Problems;
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)//Task 2
+    {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int sum = carry;
+
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10; // compute carry
+            int digit = sum % 10; // current node’s value
+
+            current.next = new ListNode(digit);
+            current = current.next;
+        }
+
+        return dummyHead.next;
+    }
+}
