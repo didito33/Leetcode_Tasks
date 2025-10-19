a5467f12b3c17c323c83734b85c35273572aa0d4
Gamer <Gamer@DESKTOP-24AJLS0>
2025-10-20 00:16:59 +0300

    session 3

---
+++ Leetcode Tasks/Problems/12.Integer to Roman.cs
@@ -0,0 +1,34 @@
+﻿using Leetcode_Tasks.Models;
+using System;
+
+namespace Leetcode_Tasks.Problems;
+public partial class Solution
+{
+    public string IntToRoman(int num)
+    {
+        string res = string.Empty;
+        while (num > 0)
+        {
+            (string symbol, int value) = num switch
+            {
+                >= 1000 => ("M", 1000),
+                >= 900 => ("CM", 900),
+                >= 500 => ("D", 500),
+                >= 400 => ("CD", 400),
+                >= 100 => ("C", 100),
+                >= 90 => ("XC", 90),
+                >= 50 => ("L", 50),
+                >= 40 => ("XL", 40),
+                >= 10 => ("X", 10),
+                9 => ("IX", 9),
+                >= 5 => ("V", 5),
+                4 => ("IV", 4),
+                _ => ("I", 1)
+            };
+
+            res += symbol;
+            num -= value;
+        }
+        return res;
+    }
+}
---
+++ Leetcode Tasks/Problems/13.Roman to Integer.cs
@@ -0,0 +1,39 @@
+﻿using Leetcode_Tasks.Models;
+using System;
+
+namespace Leetcode_Tasks.Problems;
+public partial class Solution
+{
+    public int RomanToInt(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return 0;
+
+        int GetValue(char c) => c switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0
+        };
+
+        int total = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int current = GetValue(s[i]);
+            int next = (i + 1 < s.Length) ? GetValue(s[i + 1]) : 0;
+
+            if (current < next)
+                total -= current;//Example: MCMXCIV = 1000 + 900 + 90 + 4
+            else
+                total += current;
+        }
+
+        return total;
+    }
+}
