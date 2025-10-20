98503c49b6a0c634a90cdeb78d91a0215e1bc33c
Gamer <Gamer@DESKTOP-24AJLS0>
2025-10-20 23:47:49 +0300

    session 4

---
+++ Leetcode Tasks/Problems/14.Longest Common Prefix.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode_Tasks.Models;
+using System;
+
+namespace Leetcode_Tasks.Problems;
+public partial class Solution
+{
+    public string LongestCommonPrefix(string[] strs) //["flower","flow","flight"]
+    {
+        var strResult = string.Empty;
+        if (strs == null || strs.Length == 0)
+            return strResult;
+        var minLength = strs.Select(s => s.Length).Min();
+        for (int i = 0; i < minLength;)//characters of each string
+        {
+            for (int j = 0; j < strs.Length; j++)//strings
+            {
+                var prefix = strs[j].Substring(i, 1);
+                if (strs.All(s => s.Substring(i, 1) == prefix))
+                {
+                    strResult += prefix;
+                    if(i == minLength - 1)
+                        return strResult;
+                    i++;
+                    continue;
+                }
+                return strResult;
+            }
+        }
+        return strResult;//"fl"
+    }
+}
---
+++ Leetcode Tasks/Problems/15.3Sum.cs
@@ -0,0 +1,47 @@
+﻿using Leetcode_Tasks.Models;
+using System;
+
+namespace Leetcode_Tasks.Problems;
+public partial class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        Array.Sort(nums); // sorting makes it easy to avoid duplicates
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0)
+                {
+                    result.Add([nums[i], nums[left], nums[right]]);
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;//avoid dupes
+                    while (left < right && nums[right] == nums[right + 1]) right--;// -;-
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
